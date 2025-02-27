a6f4c69baeaed306d822a840203bcae7b32063fb
Dhana Priya Tharsini <dhanapriya511@gmail.com>
2025-02-27 12:02:05 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,17 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:go_router/go_router.dart';
-import 'package:health_wallet/common/constants.dart';
-import 'package:health_wallet/common/ui_helper';
-import 'package:health_wallet/create_account/view/create_account_mobile.dart';
-import 'package:health_wallet/verification/verification.dart';
-import 'package:health_wallet/verification/verified_page.dart';
-=======
 
-import 'package:go_router/go_router.dart';
-import 'package:health_wallet/common/constants.dart';
-import 'package:health_wallet/common/ui_helper';
 
-import 'package:health_wallet/login/views/login_mobile.dart';
-import 'package:health_wallet/splash_screen.dart/splash_screen_mobile.dart';
->>>>>>> fcb8d55f66ffcc8217e161d7b6c3912c3f2ae7d5
-import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
-  final GoRouter _router = GoRouter(
-<<<<<<< HEAD
-    initialLocation: '/verified',
-    routes: [
-      GoRoute(
-        path: '/account',
-        builder: (context, state) => const CreateAccount(),
-      ),
-      GoRoute(
-        path: '/verify',
-        builder: (context, state) => const VerificationPage(),
-      ),
-      GoRoute(
-        path: '/verified',
-        builder: (context, state) => const VerifiedPage(),
-      ),
-    ],
-=======
-    initialLocation: '/splash',
-    routes: [
-      GoRoute(
-        path: '/login',
-        builder: (context, state) => const LoginMobile(),
-      ),
-      GoRoute(
-        path: '/splash',
-        builder: (context, state) => SplashScreenMobile(),
-      ),
-    ]
-
->>>>>>> fcb8d55f66ffcc8217e161d7b6c3912c3f2ae7d5
-  );
   @override
   Widget build(BuildContext context) {
-    UiHelper uiHelper = UiHelper();
-    return MaterialApp.router(
-<<<<<<< HEAD
-=======
-
->>>>>>> fcb8d55f66ffcc8217e161d7b6c3912c3f2ae7d5
-      builder:
-          (context, child) => ResponsiveBreakpoints.builder(
-            child: child!,
-            breakpoints: [
-              const Breakpoint(start: 375, end: 450, name: MOBILE),
-              const Breakpoint(start: 451, end: 800, name: TABLET),
-              const Breakpoint(start: 801, end: 1920, name: DESKTOP),
-              const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-            ],
-          ),
-      routerConfig: _router,
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: uiHelper.themeData(Constants.themeConfig.DARK),
-<<<<<<< HEAD
-=======
-
-    
-     
->>>>>>> fcb8d55f66ffcc8217e161d7b6c3912c3f2ae7d5
-    );
+   
+    return MaterialApp.router();
   }
 }
